659ce272862b640d9fb1aad645b5912a2ef653e3
john hattori <johnmhattori@yahoo.com>
2014-08-27 14:18:11 -0700

    first commit

---
+++ old_hw02/php/greeting.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+    <body class="bootstrap-frm">
+    <label>
+        <span>Welcome <?php echo $_POST["name"]; ?></span><br>
+        <span>Your email address is: <?php echo $_POST["email"]; ?></span>
+    </span>
+    </body>
+</html>
