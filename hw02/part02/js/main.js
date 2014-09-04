9d37b471f8b88381cbb43cca43a8bcea820ff81d
john hattori <johnmhattori@yahoo.com>
2014-09-04 12:07:56 -0700

    part2 functional

--- hw02/part02/js/main.js
+++ hw02/part02/js/main.js
@@ -1,13 +1,15 @@
 
 
+document.getElementById("sum").onclick = function() {sum()};
+
 function sum() 
 { 
-    var fn, sn, tn result; 
+    var fn, sn, tn, result; 
     fn = parseInt(document.getElementById("num1").value, 10);
     sn = parseInt(document.getElementById("num2").value, 10);
     tn = parseInt(document.getElementById("num3").value, 10);
     result =  (fn+sn+tn); 
-    document.getElementById("result").value += result; 
+    document.getElementById("result").value = result;
 }
 
 
@@ -16,11 +18,3 @@ function sum()
 
 
 
-
-function add(numbers) {
-    var sum = 0;
-    for (var i = 0; i < numbers.length; i++) {
-        sum += numbers[i];
-    }
-    return sum;
-  }
