38dabb5f6bbc18bbc5afdf51ef09864b2636ac16
john hattori <johnmhattori@yahoo.com>
2014-09-06 12:11:04 -0700

    part02 functional

--- hw02/part02/js/main.js
+++ hw02/part02/js/main.js
@@ -1,17 +1,14 @@
-$(document).ready(function() {
-
     document.getElementById("sum").onclick = function() {sum()};
     document.getElementById("product").onclick = function() {product()};
     document.getElementById("average").onclick = function() {average()};
     document.getElementById("least").onclick = function() {least()};
     document.getElementById("most").onclick = function() {most()};
 
-    function getNumbers()
-    {
-    var fn = parseInt(document.getElementById("num1").value, 10);
-    var sn = parseInt(document.getElementById("num2").value, 10);
-    var tn = parseInt(document.getElementById("num3").value, 10);
-    return [fn, sn, tn];
+    function getNumbers() {
+        var fn = parseInt(document.getElementById("num1").value, 10);
+        var sn = parseInt(document.getElementById("num2").value, 10);
+        var tn = parseInt(document.getElementById("num3").value, 10);
+        return [fn, sn, tn];
     }
 
     function sum() 
@@ -22,50 +19,40 @@ $(document).ready(function() {
                 total += numbers[i] << 0;
         }
         document.getElementById("result").value = total;
+        return total;
     }
 
 
     function product() 
-    { 
-        var fn, sn, tn, result; 
-        fn = parseInt(document.getElementById("num1").value, 10);
-        sn = parseInt(document.getElementById("num2").value, 10);
-        tn = parseInt(document.getElementById("num3").value, 10);
-        result = (fn*sn*tn); 
-        document.getElementById("result").value = result;
+    {  
+        numbers = getNumbers();
+        total = 1;
+        for (var i = 0; i < numbers.length; i++) {
+                total *= numbers[i];
+        }
+        document.getElementById("result").value = total;
     }
 
     function average() 
     { 
-        var fn, sn, tn, result; 
-        fn = parseInt(document.getElementById("num1").value, 10);
-        sn = parseInt(document.getElementById("num2").value, 10);
-        tn = parseInt(document.getElementById("num3").value, 10);
-        result = ((fn+sn+tn)/3); 
+        numbers = getNumbers();
+        numerator = sum();
+        denominator = numbers.length;
+        result = (numerator/denominator); 
         document.getElementById("result").value = result;
     }
 
     function least() 
     { 
-        var fn, sn, tn, result; 
-        fn = parseInt(document.getElementById("num1").value, 10);
-        sn = parseInt(document.getElementById("num2").value, 10);
-        tn = parseInt(document.getElementById("num3").value, 10);
-        result = Math.min(fn, sn, tn);
+        numbers = getNumbers();
+        result = Math.min.apply(null, numbers);
         document.getElementById("result").value = result;
     }
 
     function most() 
-    { 
-        var fn, sn, tn, result; 
-        fn = parseInt(document.getElementById("num1").value, 10);
-        sn = parseInt(document.getElementById("num2").value, 10);
-        tn = parseInt(document.getElementById("num3").value, 10);
-        result = Math.max(fn, sn, tn); 
+    {  
+        numbers = getNumbers();
+        result = Math.max.apply(null, numbers);
         document.getElementById("result").value = result;
     }
-
-});
-
-
 
