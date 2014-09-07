bcc22ab7a6b04e96f5296a30e57d1db49d8c8330
john hattori <johnmhattori@yahoo.com>
2014-09-06 17:43:42 -0700

    most functionality working part03, except reset button

--- hw02/part03/js/main.js
+++ hw02/part03/js/main.js
@@ -1,8 +1,33 @@
 $(document).ready(function() {
-  
+    
+    $('#submit').click(function () {
+        var neg = 0;
+        var pos = 0;
+        var zero = 0;
+        $('input').each (function() {
+            if ($(this).val() < 0){
+                neg += 1;
+            }
+            else if ($(this).val() > 0) {
+                pos += 1;
+            }
+            else {
+                zero += 1;
+            }
 
+            });        
+        $('#result').append("You entered " + pos +  " positive numbers. " 
+                            + neg  + " negative numbers. "
+                            + "and " + zero +  " zeros");    
+    });
 
-
+    $('#reset').click(function(){
+        $('#numForm').trigger('reset');
+   });
+    
+    $('#fade').click(function(){
+        $('#result').fadeTo("slow",0.7);
+    });
 
 });
 
