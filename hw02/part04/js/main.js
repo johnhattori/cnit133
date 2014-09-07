3b60d6f44792bfd1a5ff63f906a0bd33fceff892
john hattori <johnmhattori@yahoo.com>
2014-09-06 21:52:44 -0700

    part03

--- hw02/part04/js/main.js
+++ hw02/part04/js/main.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
-    //assign the table to a variable
-    var table = document.getElementById("1thru6");
-    //title the different columns with number, square, and cube
-    var head = table.insertRow(0);
-        head.insertCell(-1).innerHTML = "  number  ";
-        head.insertCell(-1).innerHTML = "  square  ";
-        head.insertCell(-1).innerHTML = "  cube  ";
-    //run a for loop to put the squares and cubes of 1 to 6
-    for (var i = 1; i < 7; i++){
-        var holder = table.insertRow(-1);
-        holder.insertCell(-1).innerHTML = i;
-        holder.insertCell(-1).innerHTML = (i*i);
-        holder.insertCell(-1).innerHTML = (i*i*i);
+    var list1 = [1,2,3,4,5,6];
+    var list2 = [7,8,9,10];
+
+    function create_table(list){
+        table = '<tr><td>Number</td><td>Square</td><td>Cube</td></tr>'
+        $.each(list, function(i, v) {
+            table += '<tr><td>' + v + '</td><td>' + v*v +'</td><td>' + v*v*v + '</td></tr>'
+        });
+        return table;
     }
-)};
+     
+    $('#show_table2').click(function() {
+        $('#table2').html(create_table(list2));
+    });
+
+    $('#table1').html(create_table(list1));
+});
