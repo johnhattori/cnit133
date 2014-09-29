c2cf7170bf357333e01c0a84b2e4612a45a8b642
john hattori <johnmhattori@yahoo.com>
2014-09-28 18:28:22 -0700

    part 6 functional

--- hw03/part06/js/script.js
+++ hw03/part06/js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     
-    op1 = Math.floor(Math.random() * 10);
-    op2 = Math.floor(Math.random() * 10);
     
     $('#b').click(function(){
         $('#prob').text('');    
         $('#ans').val('');
-        $('#prob').text("How much is " + op1 + " times " + op2 + "?");    
+        operands = getRandom();
+        $('#prob').text("How much is " + operands[0] + " times " + operands[1] + "?");    
+        $('#b').text('Click for multiplication problem');
     });    
    
     $('#a').click(function() {
@@ -23,12 +23,21 @@ $(document).ready(function() {
 });
 
 function calc(input) {
-    while (input !== (op1 * op2)) {
+    if(input !== (op1 * op2)) {
+    $('p:last').text("No. Please try again.");
+        
+    }else { 
         $('#ans').val(''); 
-        $('p:last').text("Try again.");
-        calc(input);
-    } 
-    $('#ans').val(''); 
-    $('p:last').text("Yup");
-    return;
-}    
+        $('p:last').text("Very good!");
+        $('#prob').text('');
+        $('#b').text('');
+        $('#b').text('click to try another problem');
+        return;
+    }
+}
+
+function getRandom() {
+    op1 = Math.floor(Math.random() * 10);
+    op2 = Math.floor(Math.random() * 10);
+    return [op1, op2]
+}
