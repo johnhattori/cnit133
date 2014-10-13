9c727081cabdc1746f24f47376a4033da4ad2202
john hattori <johnmhattori@yahoo.com>
2014-10-13 14:52:52 -0700

    part5 functional

--- hw04/part03/js/script.js
+++ hw04/part03/js/script.js
@@ -2,7 +2,7 @@
 
 for(var i = 5; i <= 10; i++){
     document.write("<table class='table table-striped'><tr>" +
-            "<th>Year</th><th>Initial Deposit</th><th>rate(%)</th>");
+            "<th>Year</th><th>Initial Deposit</th><th>rate</th>");
     accum(1000, i);
     document.write("</table>");
 }
@@ -11,7 +11,7 @@ function accum(p,r){
     var a = [], i;
     for(i = 1; i <= 10; i++){
         a[i] = (p*Math.pow(1+(r*.01),i)).toFixed(2);
-        document.write("<tr><td>"+i+"</td><td>"+a[i]+"</td><td>"+r+"</td></tr>");
+        document.write("<tr><td>"+i+"</td><td>"+a[i]+"</td><td>"+r+ "%" + "</td></tr>");
     }
 }
 
--- hw04/part05/js/script.js
+++ hw04/part05/js/script.js
@@ -1,3 +1,46 @@
 $(document).ready(function() {
-    
+    $(".show").click(function(){
+        $(".inst").slideToggle("slow");
+    });   
+    $('#submit').click(function(){
+        if($.isNumeric($("#input").val())){
+            $('#errmsg').text('');
+            listIt();
+        }
+        else{
+            $('#errmsg').html("Please enter a 1 for an unordered list, 2 for an ordered number list or 3 for ordered letter list")
+        }
+     });
+ 
 });
+
+function validInput(numinput){
+    if (isNaN(numinput) || numinput < 0){
+        return true;
+    }
+}
+
+function lists(){
+    var list ="";
+    for(var i = 1; i <= 3; i++)
+        list = list + "<li>List item" + i + "</li>";
+    return list;
+}
+
+function choice(list){
+    switch(list){
+        case 1: list = "<h3>Unordered List</h3>`<ul type='square'>" + lists() + "</ul>";
+        break;
+        case 2: list = "<h3>Ordered Number List</h3><ol>" + lists() + "</ol>";
+        break;
+        case 3: list = "<h3>Ordered Letter List</h3><ol type='A'>" + lists() + "</ol>";
+        break;
+        default: list = "<br><p style='color:red;'>Only 1, 2 and 3 are allowed as valid input.</p>" 
+    }
+    return list;
+}
+function listIt(){
+    var value;
+    value = parseInt(document.getElementById("input").value);
+    document.getElementById("list").innerHTML = choice(value);
+}
