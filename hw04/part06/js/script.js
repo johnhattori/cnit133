4e816e1949b43b93fb912901d1972993f10567f8
john hattori <johnmhattori@yahoo.com>
2014-10-13 22:40:51 -0700

    syntax fix script.js

--- hw04/part06/js/script.js
+++ hw04/part06/js/script.js
@@ -1,11 +1,11 @@
 //while loop to create lyrics
-var i = 1, dayGift="";
+var i = 1, cum="";
     document.write("<h4 style='text-align:center'>The Twelve Days of Christmas</h4>");
 while(i <= 12){
     document.write("<p style='text-align:center'>On the " + days(i) + " day of Christmas<br>"); 
-document.write("My true love gave to me:<br/>");
-dayGift = gift(i) + dayGift;
-    document.write(dayGift);
+    document.write("My true love gave to me:<br/>");
+    cum = gift(i) + cum;
+    document.write(cum);
     document.write("" + and(i) + " partridge in a pear tree.</p><br/>");
     i++;
 }
