bf952beabcec48fea56bda6a0b8fbffaf9626486
john hattori <johnmhattori@yahoo.com>
2014-11-13 11:26:26 -0800

    part2 functional

--- hw06/part02/js/script.js
+++ hw06/part02/js/script.js
@@ -1,5 +1,15 @@
-$(document).ready(function() {
+angular.module('App', [])
+  .controller('SearchController', ['$scope', function($scope) {
 
+    $scope.countChar = function() {
+        var count = 0;
+        var input = ($scope.text + '');
+        for (var i = 0; i < input.length; i++)
+            if (input.charAt(i) === ($scope.charInput+ '')) {
+                count++;
+            }
+         return count;
 
+    };
 
-});
+  }]);
