2c6e2f077e57631f14fe03643f2a384ff1062732
john hattori <johnmhattori@yahoo.com>
2014-11-12 23:06:44 -0800

    part1 functional

--- hw06/js/script.js
+++ hw06/js/script.js
@@ -1,27 +1,23 @@
-angular.module('todoApp', [])
-  .controller('TodoController', ['$scope', function($scope) {
-    $scope.todos = [
-      {text:'learn angular', done:true},
-      {text:'build an angular app', done:false}];
+angular.module('calcApp', [])
+  .controller('CalcController', ['$scope', function($scope) {
  
-    $scope.addTodo = function() {
-      $scope.todos.push({text:$scope.todoText, done:false});
-      $scope.todoText = '';
+    $scope.Clear = function() {
+        $scope.inputNum = '';
     };
- 
-    $scope.remaining = function() {
-      var count = 0;
-      angular.forEach($scope.todos, function(todo) {
-        count += todo.done ? 0 : 1;
-      });
-      return count;
+     
+    $scope.squareNum = function() {
+        return Math.sqrt(parseFloat($scope.inputNum));
     };
- 
-    $scope.archive = function() {
-      var oldTodos = $scope.todos;
-      $scope.todos = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) $scope.todos.push(todo);
-      });
+
+    $scope.tenthNum = function() {
+        return Math.floor(parseFloat($scope.inputNum) * 10 + .5) / 10
+    };
+
+    $scope.hundredNum = function() {
+        return Math.floor(parseFloat($scope.inputNum) * 100 + .5) / 100
+    };
+
+    $scope.thousandNum = function() {
+        return Math.floor(parseFloat($scope.inputNum) * 1000 + .5) / 1000
     };
   }]);
