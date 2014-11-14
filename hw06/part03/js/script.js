7d9639a60ab41aeee169b49310e66ff717fa8534
john hattori <johnmhattori@yahoo.com>
2014-11-13 16:41:15 -0800

    part3 functional, except splitting of phone number

--- hw06/part03/js/script.js
+++ hw06/part03/js/script.js
@@ -1,5 +1,17 @@
-$(document).ready(function() {
+var app = angular.module('myApp',[])
 
+app.run(function($rootScope,$interval){
+    $rootScope.AssignedDate = Date; // 'Date' could be assigned too of course:)
+    
+    $interval(function(){
+        // nothing is required here, interval triggers digest automaticaly
+    },1000)
+})
+$( document ).ready(function() {
+    var num = $( '#mobile-number' ).intlTelInput("getCleanNumber");
+    $( '#format' ).click(function() {
+        console.log($( '#mobile-number' ).intlTelInput("getCleanNumber"));
+            
+    });
 
 });
-
